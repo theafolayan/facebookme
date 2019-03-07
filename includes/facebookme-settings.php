6d3cba5dc9ed9eb393ad59f024e57aa1a1d4bfd9
opensaucedeveloper <geekzoneguy@gmail.com>
2019-03-07 15:13:50 +0100

    fixed 'fbm_options_menu_link' not found' error

--- includes/facebookme-settings.php
+++ includes/facebookme-settings.php
@@ -1,5 +1,7 @@
 <?php 
-function fb_options_menu_link(){
+
+//Creating The Menu link
+function fbm_options_menu_link(){
     add_options_page(
     'Facebook Me Options',
     'Facebook Me Link',
@@ -8,3 +10,16 @@ function fb_options_menu_link(){
     'fbm_options_content'
     );
 }
+
+//Creating Options Page Content
+function fbm_options_content(){
+    echo 'test';
+}
+
+add_action('admin_menu', 'fbm_options_menu_link');
+
+//Registering the settings
+function fbm_register_settings(){
+    register_setting( 'fbm_settings_group', 'fbm_settings');
+}
+add_action('admin_init', 'fbm_register_settings');
