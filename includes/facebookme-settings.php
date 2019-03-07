5c43daefc72f6502b1d84c886452f83fbeb567cc
opensaucedeveloper <geekzoneguy@gmail.com>
2019-03-07 09:24:38 +0100

    Fixed linting error on options page

--- includes/facebookme-settings.php
+++ includes/facebookme-settings.php
@@ -1,9 +1,10 @@
 <?php 
 function fb_options_menu_link(){
-    add_options_page(){
+    add_options_page(
     'Facebook Me Options',
     'Facebook Me Link',
     'manage_options',
     'fbm-options',
-    }
+    'fbm_options_content'
+    );
 }
